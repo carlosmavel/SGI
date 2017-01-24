537bbfe46fc97238ccd15a18ac4918dd505c54b0
Carlos Mavel <carlosmavel@hotmail.com>
2017-01-24 14:13:10 -0200

    Atualização página inicial

---
+++ sgi/src/Test.js
@@ -0,0 +1,13 @@
+// src/Test.js
+import React, { Component } from 'react';
+
+class Test extends Component {
+  render() {
+    return(
+      <i className="fa fa-home"/>
+    );
+  }
+
+}
+
+export default Test;
