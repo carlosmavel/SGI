695947cf913f9026d313e4db201edc767463542b
Fabio Amorim <binhoamorim65@gmail.com>
2017-01-25 12:33:46 -0200

    Adicionado o componente Login

--- sgi/src/App.js
+++ sgi/src/App.js
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from './Navbar';
+import Login from './Login.js';
 
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {showLoginModal: false};
+  }
+
+
+  toggleLoginModal = () => {
+      this.setState({showLoginModal: !this.state.showLoginModal});
+  }
+
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar toggleLoginModal={this.toggleLoginModal} />
         <div className="container">
-
+          <Login toggleLoginModal={this.toggleLoginModal} showLoginModal={this.state.showLoginModal} />
         </div>
       </div>
     );
