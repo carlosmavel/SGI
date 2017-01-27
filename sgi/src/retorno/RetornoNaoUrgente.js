4724fa29042e0e8cc37c2099f1e2d893df070b43
Carlos Mavel <carlosmavel@hotmail.com>
2017-01-27 14:29:20 -0200

    texto nao urgente

--- sgi/src/retorno/RetornoNaoUrgente.js
+++ sgi/src/retorno/RetornoNaoUrgente.js
@@ -7,8 +7,8 @@ class RetornoNaoUrgente extends Component {
   render(){
     return(
       <Alert bsStyle="info">
-        <h3><strong> Sua Pressão Arterial está um pouco fora no Normal!</strong></h3>
-        <p>Pratique atividade física aliada a uma alimentação balanceada</p>
+        <h3><strong> Sua Pressão Arterial está Normal!</strong></h3>
+        <p>Pratique atividade física aliada a uma alimentação balanceada para manter sua saúde em dia</p>
         <p>
         	<Button onClick={() => {this.props.toggleCollapse()}}>Hide Alert</Button>
         </p>
