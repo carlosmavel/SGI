bacca3dab2fcefe99c35915482f1e8114c14794e
Isaque Elcio <isaqueelcio@hotmail.com>
2017-01-26 22:35:42 -0200

    Linha de dados da tabela

---
+++ sgi/src/consultaDados/ColetadadosRow.js
@@ -0,0 +1,17 @@
+// src/logadoapp/Navbar.js
+import React, { Component } from 'react';
+
+
+class ColetadadosRow extends Component {
+  render() {
+    return(
+
+    	<tr>
+        <td className={className}>{pressao.pressao}</td>
+        <td>{pressao.data}</td>
+      </tr>
+    );
+  }
+}
+
+export default PanelForm;
