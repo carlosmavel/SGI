2e6a20df4c349dfea159b34cd524076fcd8b7e63
Alisson Nunes <alynva@gmail.com>
2017-01-27 11:39:42 -0200

    Função setView alterada para dentro do LoggedContainer

--- sgi/src/LoggedContainer.js
+++ sgi/src/LoggedContainer.js
@@ -3,20 +3,41 @@ import React, { Component } from 'react';
 
 import { Grid, Row, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
+import HomeLogged from './HomeLogged';
+import HPressLogged from './consultaDados/HPressLogged';
+import HGliceLogged from './consultaDados/HGliceLogged';
+import Jumbotron from './logadoapp/Jumbotron';
+import JumboGlice from './logadoapp/JumboGlice';
+
 //import SystemDescription from './SystemDescription';
 
 class LoggedContainer extends Component {
 
-	handleSelect = (eventKey) => {
-		if (eventKey)
-			this.props.setView(eventKey);
+	constructor(props) {
+		super(props);
+		this.state = {
+			view: "initial"
+		};
+		this.views = {
+			initial: <HomeLogged />,
+			h_press: <HPressLogged />,
+			h_glice: <HGliceLogged />,
+			i_press: <Jumbotron setView={this.setView}/>,
+			i_glice: <JumboGlice setView={this.setView} />
+		}
+	}
+
+	setView = (view) => {
+		this.setState({
+			view: view
+		});
 	}
 	
 	render(){
 		return(
 			<Grid>
 				<Row>
-					<Navbar onSelect={this.handleSelect}>
+					<Navbar onSelect={this.setView}>
 						<Navbar.Header>
 							<Navbar.Brand>
 								<a href="#">Início</a>
@@ -33,7 +54,7 @@ class LoggedContainer extends Component {
 					</Navbar>
 				</Row>
 				<Row>
-					{this.props.theView}
+					{this.views[this.state.view]}
 				</Row>
 			</Grid>
 		);
--- sgi/src/LoggedView.js
+++ sgi/src/LoggedView.js
@@ -5,43 +5,13 @@ import React, { Component } from 'react';
 import LoggedNavbar from './LoggedNavbar';
 import LoggedContainer from './LoggedContainer';
 
-import HomeLogged from './HomeLogged';
-import HPressLogged from './consultaDados/HPressLogged';
-import HGliceLogged from './consultaDados/HGliceLogged';
-import Jumbotron from './logadoapp/Jumbotron';
-import JumboGlice from './logadoapp/JumboGlice';
-
-
 class LoggedView extends Component {
-
-	constructor(props) {
-		super(props);
-		this.state = {
-			view: "initial"
-		};
-		this.views = {
-			initial: <HomeLogged />,
-			h_press: <HPressLogged />,
-			h_glice: <HGliceLogged />,
-			i_press: <Jumbotron setView={this.setView}/>,
-			i_glice: <JumboGlice setView={this.setView} />
-		}
-	}
-
-	setView = (view) => {
-		this.setState({
-			view: view
-		});
-	}
 
 	render() {
 		return (
 			<div>
 				<LoggedNavbar setPage={this.props.setPage} />
-				<LoggedContainer
-					theView={this.views[this.state.view]}
-					setView={this.setView}
-				/>
+				<LoggedContainer />
 			</div>
 		);
 	};
