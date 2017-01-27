7cf6a331b5c3713074dbd9e7e962b59b89c7c160
Alisson Nunes <alynva@gmail.com>
2017-01-27 10:52:54 -0200

    Resolvido bug da classificação da pressão

--- sgi/src/consultaDados/HPressRow.js
+++ sgi/src/consultaDados/HPressRow.js
@@ -11,24 +11,24 @@ class HPressRow extends Component {
   		} = this.props;
 
 		let pre = pressao.split("/");
-		let max = Number.isInteger(pre[0] / 10) && (pre[0] !== 10) ? pre[0] / 10 : pre[0];
-		let min = Number.isInteger(pre[1] / 10) ? pre[1] / 10 : pre[1];
+		let max = (Number(pre[0]) !== 10) && Number.isInteger(pre[0] / 10) ? pre[0] / 10 : pre[0];
+		let min = (Number(pre[1]) !== 10) && Number.isInteger(pre[1] / 10) ? pre[1] / 10 : pre[1];
 
   		let classi;
   		let style;
 
-  		if (max < 10 && min <= 6) {
+  		if (max < 10 && min < 6) {
   			classi = "Hipertensão";
-  			style = "default";
+  			style = "danger";
 		} else if (max >= 10 && max < 14 && min >= 6 && min < 9) {
 			classi = "Normal";
 			style = "default";
 		} else if (max >= 14 && max < 16 && min >= 9 && min < 10) {
 			classi = "Hipertensão limite";
-			style = "default";
+			style = "success";
 		} else if (max >= 16 && max < 18 && min >= 10 && min < 11)  {
 			classi = "Hipertensão moderada";
-			style = "default";
+			style = "warning";
 		}
 
 		return(
